54cb5bca13f09a1026043500c89d975dd53e9b26
connorstafee <connorstafee@gmail.com>
2026-09-06 14:25:09 +0530

    01_target_vs_actual.sql

--- sql_queries/01_target_vs_actual.sql
+++ sql_queries/01_target_vs_actual.sql
@@ -1,7 +1,9 @@
-SELECT	Mine,
-		SUM(Target_Tonnes) AS Total_Target,
-        SUM(Actual_Tonnes) AS Total_Actual,
-        (SUM(Actual_Tonnes)/SUM(Target_Tonnes))*100 AS Achievement_Percentage
-FROM moil_data
-GROUP BY Mine
-ORDER BY Achievement_Percentage DESC;
+-- Compare actual vs target tonnes by mine
+
+SELECT mine,
+       SUM(target_tonnes) AS total_target,
+       SUM(actual_tonnes) AS total_actual,
+       ROUND(SUM(actual_tonnes)/SUM(target_tonnes)*100,2) AS achievement_pct
+FROM production
+GROUP BY mine
+ORDER BY achievement_pct DESC;
